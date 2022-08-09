ad7299823db505a4583d55910b6efeeaa242ec46
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-08-09 17:07:40 +0300

    feat: add input field to HookCounter component

--- react-hooks/src/Components/HookCounter.js
+++ react-hooks/src/Components/HookCounter.js
@@ -4,13 +4,20 @@ import { useEffect } from "react";
 
 function HookCounter() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
   useEffect(() => {
+    console.log("updating doc title");
     document.title = `You clicked ${count} times`;
-  });
+  }, [count]);
 
   return (
     <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <button onClick={() => setCount(count + 1)}>{count} times</button>
     </div>
   );
