f441818529295511c589494cd1786c1171619e40
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-08-09 14:15:29 +0300

    feat: add Counterarray componet it renders list of generated numbers stored in our state object

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
+import Counterarray from "./Components/Counterarray";
 // import Counter from "./Components/Counter";
-import CounterObject from "./CounterObject";
+// import CounterObject from "./CounterObject";
 
 function App() {
   return (
     <div className="App">
       {/* <Counter /> */}
-      <CounterObject />
+      {/* <CounterObject /> */}
+      <Counterarray />
     </div>
   );
 }
