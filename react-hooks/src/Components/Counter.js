3f1e64f9f864ad7406062ffc31d2c9ee3271a2d7
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-08-09 12:11:58 +0300

    feat: add a button that increases the count by 5

--- react-hooks/src/Components/Counter.js
+++ react-hooks/src/Components/Counter.js
@@ -2,15 +2,24 @@ import React from "react";
 import { useState } from "react";
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
 
-  const increaseCount = () => {
-    setCount(count + 1);
+  const increaseCountBy5 = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount((prevCount) => prevCount + 1);
+    }
   };
 
   return (
     <div>
-      <button onClick={increaseCount}>Count {count}</button>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increase Count
+      </button>
+      <button onClick={increaseCountBy5}>
+        Count increase by a figure by 5
+      </button>
+      <div>{count}</div>
     </div>
   );
 }
