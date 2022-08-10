e4c9256fe9291878f10baffb57acd91f0254fea7
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-08-10 07:14:16 +0300

    feat: add DataFetchingHook its aimed to fetch data using axios for study of how to fetch data using useEffect

---
+++ react-hooks/src/Components/DataFetchigHook.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+function DataFetchigHook() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        console.log(response);
+        setPosts(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {posts.map((post) => {
+          return <li key={post.id}>{post.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default DataFetchigHook;
